56f90cb833f62455e16840ab34a7e8e2791497a6
FannyNh <thethirdmoira@gmail.com>
2021-10-08 18:03:40 +0200

    Revert "corps issue"
    This reverts commit 3a55d133

--- axios.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-const config = { headers: { "Access-Control-Allow-Origin": "*", "Content-Type": "text/plain", }, }; const instance = axios.create({ baseURL: "https://api.themoviedb.org/3", https: config, });
-
-export default instance;
